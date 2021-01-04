aa5a4affc1c4a43a02b60272d6c6406bcc74413e
rcalvom <rcalvom@unal.edu.co>
2021-01-03 19:42:33 -0500

    Avances en comandos

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
+// https://discord.js.org/#/docs/main/stable/general/welcome
 const Discord = require('discord.js');
 const config = require('./config.json');
 
 
-const client = new Discord.Client();
+const bot = new Discord.Client();
+
+const prefix = "J!";
+
+bot.once('ready', () =>{
+    console.log("Logged in as " + bot.user.tag + "!");
+});
+
+bot.on('message', function name(message) {
+   if(message.author.bot) return;
+   if(!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    switch(command){
+        case "joke": {
+            message.channel.send("prueba de tts.", {
+                tts: true
+            });
+            break;
+        }
+        case "ping": {
+            message.reply("Pong! La latencia es de " + (Date.now() - message.createdTimestamp) + "ms.");
+            break;
+        }
+        default: {
+            message.reply("El comando \"" + command + "\" no existe.");
+            break;
+        }
+    }
+
+});
 
 
-client.once('ready', () =>{
-    console.log('online!');
-})
+bot.login(config.BOT_TOKEN);
 
-try{
-    client.login('Nzk1MzQ1ODgwMjU2ODcyNDY5.X_IBkg.KIwJKx97ymnnnyEXjx76umHDp5A');
-}catch{
-
-}
 
 
 
